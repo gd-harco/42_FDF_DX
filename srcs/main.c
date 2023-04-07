5eb6b4a8d73e0fea61c086caf70a839d5c62f459
gd-harco <gd-harco@student.42lyon.fr>
2023-04-07 16:43:29 +0200

    removed garbage function in main.c

--- srcs/main.c
+++ srcs/main.c
@@ -12,26 +12,6 @@
 
 #include "fdf.h"
 
-#include <stdio.h>
-void print_map(t_vec3d **map)
-{
-	int row;
-	int col;
-
-	row = 0;
-	while (row <= 9)
-	{
-		col = 0;
-		while (col <= 19)
-		{
-			printf("%d ", (int)map[row][col].z);
-			col++;
-		}
-		printf("\n");
-		row++;
-	}
-}
-
 int	main(int argc, char **argv)
 {
 	t_fdf	*fdf_data;
@@ -42,11 +22,8 @@ int	main(int argc, char **argv)
 		exit(1);
 	}
 	fdf_data = fdf_init(argv[1]);
-	//TODO remove this
-	print_map(fdf_data->map->map_base);
 	project_view(fdf_data);
-    print_map(fdf_data->map->map_projected);
-//	draw_all(fdf_data);
+	draw_all(fdf_data);
 	mlx_loop(fdf_data->mlx_win->mlx);
 	(void)fdf_data;
 	return (0);
