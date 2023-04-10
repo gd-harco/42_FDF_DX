4ab1682d5ced4305c563420ed3c83a4a768300bc
gd-harco <gd-harco@student.42lyon.fr>
2023-04-10 16:03:13 +0200

    fixed leaks of t_line arrays

--- srcs/drawing.c
+++ srcs/drawing.c
@@ -7,19 +7,31 @@ void	draw_all(t_fdf *fdf_data)
 {
 	t_nlx_line	**all_line;
 	int			nb_line;
+	int			i;
 
+	i = 0;
 	all_line = get_all_line(fdf_data->map,
 			fdf_data->map->map_projected, &nb_line);
 	while (nb_line)
 	{
-		nlx_draw_line(&fdf_data->img, *all_line, COLOR_BLUE);
-		all_line++;
+		nlx_draw_line(&fdf_data->img, all_line[i++], COLOR_BLUE);
+		free(all_line[i - 1]);
 		nb_line--;
 	}
+	free(all_line);
 	mlx_put_image_to_window(fdf_data->mlx_win->mlx,
 		fdf_data->mlx_win->win_ptr, fdf_data->img.img_ptr, 0, 0);
 }
 
+/**
+ * @brief Get all the line to draw
+ * @details The function create a t_nlx_line for each line to draw
+ * @param map the map structure
+ * @param dmap the map projected
+ * @param nb_line the number of line to draw
+ * @allocated_on Heap (must be freed)
+ * @return t_nlx_line** the array of t_nlx_line
+ */
 t_nlx_line	**get_all_line(t_map *map, t_vec3d **dmap, int *nb_line)
 {
 	t_nlx_line	**lines;
@@ -29,6 +41,8 @@ t_nlx_line	**get_all_line(t_map *map, t_vec3d **dmap, int *nb_line)
 
 	*nb_line = 2 * map->height * map->width - map->width - map->height;
 	lines = malloc(sizeof(t_nlx_line) * (*nb_line));
+	if (!lines)
+		return (NULL);
 	row = 0;
 	i = 0;
 	while (row < map->height)
