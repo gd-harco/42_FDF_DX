b2f38124afeb955c2912fd4d40f18b1a5f0f3994
gd-harco <gd-harco@student.42lyon.fr>
2023-04-11 16:08:19 +0200

    moved all initialisation in the function get_world

--- srcs/init.c
+++ srcs/init.c
@@ -22,6 +22,11 @@
 
 #include "fdf.h"
 
+static void	get_proj(t_fdf *fdf_data);
+static void	get_world(t_fdf *fdf_data);
+static void	init_translate(t_fdf *fdf_data);
+static void	init_rotate(t_fdf *fdf_data);
+
 /**
  * @brief Initialize the fdf structure.
  * @details Initialize the fdf structure by calling the needed nlx functions
@@ -41,8 +46,19 @@ t_fdf	*fdf_init(char *file)
 		free(fdf_data);
 		exit (3);
 	}
-	fdf_data->world = malloc(sizeof(t_world_i));
-	fdf_data->mlx_win = (t_win *)malloc(sizeof(t_win));
+	fdf_data->mlx_win = malloc(sizeof(t_win));
+	fdf_data->world = malloc(sizeof (t_world_i));
+	if (!fdf_data->mlx_win || !fdf_data->world)
+		return (NULL);
+	get_world(fdf_data);
+	get_proj(fdf_data);
+	nlx_win_init(fdf_data->mlx_win, WIDTH, HEIGHT, "FDF");
+	nlx_new_image(&fdf_data->img, fdf_data->mlx_win->mlx, WIDTH, HEIGHT);
+	return (fdf_data);
+}
+
+static void	get_proj(t_fdf *fdf_data)
+{
 	fdf_data->world->proj->aspect_ratio = (float)HEIGHT / (float)WIDTH;
 	fdf_data->world->proj->fov = 90.0f;
 	fdf_data->world->proj->z_near = 0.1f;
@@ -50,7 +66,47 @@ t_fdf	*fdf_init(char *file)
 	fdf_data->world->proj->fov_rad = 1.0f
 		/ tanf(fdf_data->world->proj->fov * 0.5f / 180.0f * (float)M_PI);
 	fdf_data->world->proj->m = get_projection_matrix(fdf_data->world->proj);
-	nlx_win_init(fdf_data->mlx_win, WIDTH, HEIGHT, "FDF");
-	nlx_new_image(&fdf_data->img, fdf_data->mlx_win->mlx, WIDTH, HEIGHT);
-	return (fdf_data);
+}
+
+static void	get_world(t_fdf *fdf_data)
+{
+	fdf_data->world->trans = malloc(sizeof (t_trans_info));
+	fdf_data->world->rot = malloc(sizeof (t_rot_info));
+	fdf_data->world->proj = malloc(sizeof (t_proj_info));
+	fdf_data->world->world_m = malloc(sizeof (t_matrix));
+	if (fdf_data->world->trans == NULL
+		|| fdf_data->world->rot == NULL
+		|| fdf_data->world->proj == NULL
+		|| fdf_data->world->world_m == NULL)
+		exit(0);
+	init_translate(fdf_data);
+	init_rotate(fdf_data);
+	fdf_data->world->world_m = get_world_matrix(fdf_data->world);
+}
+
+static void	init_translate(t_fdf *fdf_data)
+{
+	if (fdf_data->map->width > fdf_data->map->height
+		&& fdf_data->map->width > fdf_data->map->highest_point)
+		fdf_data->world->trans->translate_z = fdf_data->map->width;
+	else if (fdf_data->map->height > fdf_data->map->highest_point)
+		fdf_data->world->trans->translate_z = fdf_data->map->height;
+	else
+		fdf_data->world->trans->translate_z = fdf_data->map->highest_point;
+	fdf_data->world->trans->translate_x = 0.0f;
+	fdf_data->world->trans->translate_y = 2.5f;
+	fdf_data->world->trans->m = get_translation_matrix(fdf_data->world);
+}
+
+static void	init_rotate(t_fdf *fdf_data)
+{
+	fdf_data->world->rot->rot_x = (-(M_PI/4.0f));
+	fdf_data->world->rot->rot_y = 0.0f;
+	fdf_data->world->rot->rot_z = 0.0f;
+	fdf_data->world->rot->x_rot_m
+		= get_x_rotation_matrix(fdf_data->world->rot->rot_x);
+	fdf_data->world->rot->y_rot_m
+		= get_y_rotation_matrix(fdf_data->world->rot->rot_y);
+	fdf_data->world->rot->z_rot_m
+		= get_z_rotation_matrix(fdf_data->world->rot->rot_z);
 }
--- srcs/projections.c
+++ srcs/projections.c
@@ -14,8 +14,6 @@
 #include "init.h"
 #include <math.h>
 
-static void	get_world(t_fdf *fdf_data);
-
 static void	scale_in_view(t_vec3d *to_scale);
 
 void	project_view(t_fdf *fdf_data)
@@ -26,17 +24,16 @@ void	project_view(t_fdf *fdf_data)
 
 	row = 0;
 	fdf_data->map->highest_point = -fdf_data->map->highest_point;
-	get_world(fdf_data);
 	while (row < fdf_data->map->height)
 	{
 
 		col = 0;
 		while (col < fdf_data->map->width)
 		{
-			multiply_vector_matrix(fdf_data->world.world,
+			multiply_vector_matrix(fdf_data->world->world_m,
 				&fdf_data->map->map_base[row][col],
 				&tmp);
-			multiply_vector_matrix(fdf_data->world.proj->m,
+			multiply_vector_matrix(fdf_data->world->proj->m,
 				&tmp,
 				&fdf_data->map->map_projected[row][col]);
 			scale_in_view(&fdf_data->map->map_projected[row][col]);
@@ -61,22 +58,4 @@ static void	scale_in_view(t_vec3d *to_scale)
 	vector_divide(to_scale, to_scale->w);
 	to_scale->x = (to_scale->x + 1.0f) * (float)WIDTH / 2.0f + 0.5f;
 	to_scale->y = (to_scale->y + 1.0f) * (float)HEIGHT / 2.0f + 0.5f;
-}
-
-
-void	get_world(t_fdf *fdf_data)
-{
-	if (fdf_data->map->width > fdf_data->map->height
-		&& fdf_data->map->width > fdf_data->map->highest_point)
-		fdf_data->world.trans->translate_z = fdf_data->map->width;
-	else if (fdf_data->map->height > fdf_data->map->highest_point)
-		fdf_data->world.trans->translate_z = fdf_data->map->height;
-	else
-		fdf_data->world.trans->translate_z = fdf_data->map->highest_point;
-	fdf_data->world.trans->translate_x = 0.0f;
-	fdf_data->world.trans->translate_y = 2.5f;
-	fdf_data->world.rot->rot_x = (-(M_PI/4.0f));
-	fdf_data->world.rot->rot_y = 0.0f;
-	fdf_data->world.rot->rot_z = 0.0f;
-	fdf_data->world.world = get_world_matrix(&fdf_data->world);
 }
