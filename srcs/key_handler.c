8f5d8ee511452a72c1f0ef70fe84ad5529829b2a
gd-harco <gd-harco@student.42lyon.fr>
2023-04-11 18:01:56 +0200

    tested to implement translation, currently crash everytime

--- srcs/key_handler.c
+++ srcs/key_handler.c
@@ -5,9 +5,9 @@ int	key_handler(int key, t_fdf *fdf_data)
 {
 	if (key == XK_Escape || key == XK_q)
 		exit_program(fdf_data);
-//	else if (key == XK_Left || key == XK_Right
-//		|| key == XK_Up || key == XK_Down)
-//		translation(key, fdf_data);
+	else if (key == XK_Left || key == XK_Right
+		|| key == XK_Up || key == XK_Down)
+		translation(key, fdf_data);
 	else if (key == XK_a || key == XK_d
 		|| key == XK_w || key == XK_s)
 		rotation(key, fdf_data);
@@ -21,7 +21,19 @@ int	key_handler(int key, t_fdf *fdf_data)
 
 int	exit_program(t_fdf *fdf_data)
 {
+	mlx_destroy_image(fdf_data->mlx_win->mlx, fdf_data->img.img_ptr);
 	mlx_destroy_window(fdf_data->mlx_win->mlx, fdf_data->mlx_win->win_ptr);
+	mlx_destroy_display(fdf_data->mlx_win->mlx);
+	free(fdf_data->world->proj->m);
+	free(fdf_data->world->proj);
+	free(fdf_data->world->trans->m);
+	free(fdf_data->world->trans);
+	free(fdf_data->world->rot->x_rot_m);
+	free(fdf_data->world->rot->y_rot_m);
+	free(fdf_data->world->rot->z_rot_m);
+	free(fdf_data->world->rot);
+	free(fdf_data->world->world_m);
+	free(fdf_data->world);
 	exit(0);
 }
 
@@ -43,23 +55,23 @@ void	rotation(int key, t_fdf *fdf_data)
 	draw_all(fdf_data);
 }
 
-//void	translation(int key, t_fdf *fdf_data)
-//{
-//	if (key == XK_Up)
-//		fdf_data->world->trans->translate_x -= 0.25f;
-//	else if (key == XK_Down)
-//		fdf_data->world->trans->translate_x += 0.25f;
-//	else if (key == XK_Left)
-//		fdf_data->world->trans->translate_y -= 0.25f;
-//	else if (key == XK_Right)
-//		fdf_data->world->trans->translate_y += 0.25f;
-//	mlx_destroy_image(fdf_data->mlx_win->mlx, fdf_data->img.img_ptr);
-//	nlx_new_image(&fdf_data->img, fdf_data->mlx_win->mlx, WIDTH, HEIGHT);
-//	update_translation(fdf_data->world->trans);22
-//	update_world(fdf_data->world);
-//	project_view(fdf_data);
-//	draw_all(fdf_data);
-//}
+void	translation(int key, t_fdf *fdf_data)
+{
+	if (key == XK_Up)
+		fdf_data->world->trans->translate_x -= 0.25f;
+	else if (key == XK_Down)
+		fdf_data->world->trans->translate_x += 0.25f;
+	else if (key == XK_Left)
+		fdf_data->world->trans->translate_y -= 0.25f;
+	else if (key == XK_Right)
+		fdf_data->world->trans->translate_y += 0.25f;
+	mlx_destroy_image(fdf_data->mlx_win->mlx, fdf_data->img.img_ptr);
+	nlx_new_image(&fdf_data->img, fdf_data->mlx_win->mlx, WIDTH, HEIGHT);
+	update_translation(fdf_data->world->trans);
+	update_world(fdf_data->world);
+	project_view(fdf_data);
+	draw_all(fdf_data);
+}
 
 void	change_fov(int key, t_fdf *fdf_data)
 {
