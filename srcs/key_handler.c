982d917cf8bf5c190dffcf9d8e00404b9207baeb
gd-harco <gd-harco@student.42lyon.fr>
2023-04-11 17:33:29 +0200

    Implemented first draft of FOV change, still need bugfixes, can randomly crash

--- srcs/key_handler.c
+++ srcs/key_handler.c
@@ -11,9 +11,9 @@ int	key_handler(int key, t_fdf *fdf_data)
 	else if (key == XK_a || key == XK_d
 		|| key == XK_w || key == XK_s)
 		rotation(key, fdf_data);
-//	else if (key == XK_1 || key == XK_2
-//		|| key == XK_3 || key == XK_4)
-//		change_fov(key, fdf_data);
+	else if (key == XK_1 || key == XK_2
+		|| key == XK_3 || key == XK_4)
+		change_fov(key, fdf_data);
 //	else if (key == XK_plus || key == XK_minus)
 //		zoom(key, fdf_data);
 	return (0);
@@ -55,8 +55,27 @@ void	rotation(int key, t_fdf *fdf_data)
 //		fdf_data->world->trans->translate_y += 0.25f;
 //	mlx_destroy_image(fdf_data->mlx_win->mlx, fdf_data->img.img_ptr);
 //	nlx_new_image(&fdf_data->img, fdf_data->mlx_win->mlx, WIDTH, HEIGHT);
-//	update_translation(fdf_data->world->trans);
+//	update_translation(fdf_data->world->trans);22
 //	update_world(fdf_data->world);
 //	project_view(fdf_data);
 //	draw_all(fdf_data);
 //}
+
+void	change_fov(int key, t_fdf *fdf_data)
+{
+	if (key == XK_2)
+		fdf_data->world->proj->fov = 90.0f;
+	else if (key == XK_1)
+		fdf_data->world->proj->fov = 60.0f;
+	else if (key == XK_3)
+		fdf_data->world->proj->fov = 120.0f;
+	else if (key == XK_4)
+		fdf_data->world->proj->fov = 150.0f;
+	fdf_data->world->proj->fov_rad = 1.0f
+		/ tanf(fdf_data->world->proj->fov * 0.5f / 180.0f * (float) M_PI);
+	mlx_destroy_image(fdf_data->mlx_win->mlx, fdf_data->img.img_ptr);
+	nlx_new_image(&fdf_data->img, fdf_data->mlx_win->mlx, WIDTH, HEIGHT);
+	update_projection(fdf_data->world->proj);
+	project_view(fdf_data);
+	draw_all(fdf_data);
+}
