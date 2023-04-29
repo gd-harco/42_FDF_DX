7190526f48f5968b6cf1aa19e5402eb5fb5d2dd6
gd-harco <gd-harco@student.42lyon.fr>
2023-04-29 11:16:42 +0200

    updated nlx.h to reflect change in newlibx

--- includes/nlx.h
+++ includes/nlx.h
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 15:49:46 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/03/27 16:18:35 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2023/04/28 18:23:24 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include "../lib/new_libx/includes/nlx_mat.h"
 # include "../lib/new_libx/includes/nlx_vec.h"
 # include "../lib/new_libx/includes/nlx_win.h"
+# include "../lib/new_libx/includes/nlx_draw.h"
 # include "../lib/new_libx/minilibx_linux/mlx.h"
 
 #endif
